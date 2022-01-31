2699cc11cf5c71e92798b65da589ecbbc23dddbc
lolo78965 <frobert.loick0@gmail.com>
2022-01-31 18:39:20 +0000

    oui

--- main/02-intermediaire.js
+++ main/02-intermediaire.js
@@ -3,7 +3,7 @@
  * 
  * Exemple : [1, 2, 3] et ["apple", "orange", "banana"] renverra : [1, "apple", 2, "orange", 3, "banana"]
  */
-function combine(tableau1, tableau2) {
+ function combine(tableau1, tableau2) {
     var newTable = [];
     if (tableau1.length !== tableau2.length) return
     for (var i = 0; i < tableau1.length; i++) {
@@ -15,7 +15,6 @@ function combine(tableau1, tableau2) {
 var tableau1Combine = [1, 2, 3, 4];
 var tableau2Combine = ['apple', 'banana', 'orange', 'lemon'];
 console.log(combine(tableau1Combine, tableau2Combine));
-}
 
 /**
  * Programmer une fonction prenant en argument un tableau d'éléments et une valeur offset, et qui renvoie un tableau avec les valeurs du tableau en argument décalées de la valeur
@@ -35,8 +34,6 @@ function rotate(tableau, offset) {
 var tableauRotate = [1, 2, 3, 4, 5, 6, 7, 8];
 console.log(rotate(tableauRotate, 2));
 
-}
-
 /**
  * Suite de Syracuse
  * 
@@ -49,5 +46,18 @@ console.log(rotate(tableauRotate, 2));
  * Pour 1, la suite sera : [1, 4, 2, 1, 4, 2, 1, 4, 2, 1]
  */
 function syracuse(nombre) {
-
+    var currentNbre = nombre;
+    var newTable = [];
+    newTable[0] = currentNbre;
+    for (var i = 1; i < 10; i++) {
+        if (currentNbre % 2 === 0) {
+            currentNbre /= 2;
+        }else {
+            currentNbre = currentNbre * 3 + 1;
+        }
+        newTable[i] = currentNbre;
+    }
+    return newTable;
 }
+var syracuseNbre = 14;
+console.log(syracuse(syracuseNbre));
